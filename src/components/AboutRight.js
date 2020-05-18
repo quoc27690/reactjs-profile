b1a3e664c5f72283103a5ef073e5532f35efa360
quoc27690 <quoc27690@gmail.com>
2020-05-18 07:41:02 +0700

    fix responsive

--- src/components/AboutRight.js
+++ src/components/AboutRight.js
@@ -11,7 +11,7 @@ export default function AboutRight() {
     <Slide right big delay={1000}>
       <div className="ml-3">
         <br /> <br />
-        <div className="d-flex mb-3">
+        <div className="row mb-3">
           <img src={person} width="30" alt="" />
           <h4 className="text-danger ml-2" style={{ marginBottom: 0 }}>
             SUMMARIZE MYSELF
@@ -28,13 +28,13 @@ export default function AboutRight() {
             </li>
           </ul>
         </div>
-        <div className="d-flex mb-3">
+        <div className="row mb-3">
           <img src={timeline} width="30" alt="" />
           <h4 className="text-danger ml-2" style={{ marginBottom: 0 }}>
             TIMELINE
           </h4>
         </div>
-        <div className="d-flex">
+        <div className="row">
           <div style={{ width: 500 }}>
             <p className="font-weight-bold" style={{ marginBottom: 0 }}>
               THE UNIVERSITY OF DA NANG - UNIVERSITY OF SCIENCE AND TECHNOLOGY
@@ -45,7 +45,7 @@ export default function AboutRight() {
             <p>07/2008 - 07/2013</p>
           </div>
         </div>
-        <div className="d-flex">
+        <div className="row">
           <div style={{ width: 500 }}>
             <p className="font-weight-bold" style={{ marginBottom: 0 }}>
               B.R CONSULTING DESIGN CO., LTD. IN HO CHI MINH CITY
@@ -64,7 +64,7 @@ export default function AboutRight() {
             <p>08/2013 - 11/2019</p>
           </div>
         </div>
-        <div className="d-flex">
+        <div className="row">
           <div style={{ width: 500 }}>
             <p className="font-weight-bold" style={{ marginBottom: 0 }}>
               JOINED REACT COURSES AT IMIC TECHNOLOGY
@@ -81,7 +81,7 @@ export default function AboutRight() {
             <p>11/2019 - 02/2020</p>
           </div>
         </div>
-        <div className="d-flex">
+        <div className="row">
           <div style={{ width: 500 }}>
             <p className="font-weight-bold" style={{ marginBottom: 0 }}>
               JOINED CODERSX 2020 AT CODERSX
@@ -98,7 +98,7 @@ export default function AboutRight() {
             <p>02/2019 - 05/2020</p>
           </div>
         </div>
-        <div className="d-flex mb-3">
+        <div className="row mb-3">
           <img src={music} width="30" alt="" />
           <h4 className="text-danger ml-2" style={{ marginBottom: 0 }}>
             INTERESTS
