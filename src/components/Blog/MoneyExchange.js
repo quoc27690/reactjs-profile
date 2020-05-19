26e71ef2affdbce05e714b125943101241727985
quoc27690 <quoc27690@gmail.com>
2020-05-19 20:53:59 +0700

    add-endpoint-exchange

--- src/components/Blog/MoneyExchange.js
+++ src/components/Blog/MoneyExchange.js
@@ -4,6 +4,8 @@ import Header from "../Header";
 
 import Slide from "react-reveal/Slide";
 
+// const axios = require("axios");
+
 const exchangeRate = {
   VND: {
     USD: 0.000043,
@@ -18,6 +20,12 @@ const exchangeRate = {
 function exchange(amount, from, to) {
   return exchangeRate[from][to] * amount;
 }
+
+// function exchange(amount, from, to) {
+//   return axios.get(
+//     `https://nodejs-express-demo-quoc276.herokuapp.com/api/exchange?amount=${amount}&from=${from}&to=${to}`
+//   );
+// }
 
 export default function MoneyExchange() {
   const [amount, setAmount] = useState(0);
@@ -35,29 +43,29 @@ export default function MoneyExchange() {
             <div className="col"></div>
             <div className="col">
               <form>
-                <div class="form-group">
+                <div className="form-group">
                   <label>Amount</label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     onChange={(e) => setAmount(e.target.value)}
                     value={amount}
                   />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                   <label>From</label>
                   <select
-                    class="form-control"
+                    className="form-control"
                     onChange={(e) => setFrom(e.target.value)}
                   >
                     <option value="VND">VND</option>
                     <option value="USD">USD</option>
                   </select>
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                   <label>To</label>
                   <select
-                    class="form-control"
+                    className="form-control"
                     onChange={(e) => setTo(e.target.value)}
                   >
                     <option value="VND">VND</option>
@@ -65,14 +73,22 @@ export default function MoneyExchange() {
                     <option value="JPY">JPY</option>
                   </select>
                 </div>
-                <div class="form-group text-center">
+                <div className="form-group text-center">
                   <button
-                    class="form-control btn btn-primary"
+                    className="form-control btn btn-primary"
                     onClick={(e) => {
                       e.preventDefault();
                       const returnedValue = exchange(amount, from, to);
                       setReturnedAmount(returnedValue);
                     }}
+
+                    // Call API
+                    // onClick={async (e) => {
+                    //   e.preventDefault();
+                    //   const res = await exchange(amount, from, to);
+                    //   const { returnedAmount } = res.data;
+                    //   setReturnedAmount(returnedAmount);
+                    // }}
                   >
                     Convert
                   </button>
